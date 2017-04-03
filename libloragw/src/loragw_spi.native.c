249e5a2ed85efdef3a6779b91a9ec3cb50d71cbe
Stéphane David <stephane.david@1-gate.com>
2017-04-03 11:56:12 +0200

    Update loragw_spi.native.c

--- libloragw/src/loragw_spi.native.c
+++ libloragw/src/loragw_spi.native.c
@@ -53,7 +53,7 @@ Maintainer: Sylvain Miermont
 
 #define READ_ACCESS     0x00
 #define WRITE_ACCESS    0x80
-#define SPI_SPEED       8000000
+#define SPI_SPEED       1000000
 #define SPI_DEV_PATH    "/dev/spidev0.0"
 //#define SPI_DEV_PATH    "/dev/spidev32766.0"
 
