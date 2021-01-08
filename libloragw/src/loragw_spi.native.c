13137065dc108a632a2dbb78c0533eea9425e78f
Ryan Walmsley <Ryan@pi-supply.com>
2021-01-08 13:01:47 +0000

    Change SPI port

--- libloragw/src/loragw_spi.native.c
+++ libloragw/src/loragw_spi.native.c
@@ -53,8 +53,8 @@ Maintainer: Sylvain Miermont
 
 #define READ_ACCESS     0x00
 #define WRITE_ACCESS    0x80
-#define SPI_SPEED       8000000
-#define SPI_DEV_PATH    "/dev/spidev0.0"
+#define SPI_SPEED       2000000
+#define SPI_DEV_PATH    "/dev/spidev1.2"
 //#define SPI_DEV_PATH    "/dev/spidev32766.0"
 
 /* -------------------------------------------------------------------------- */
